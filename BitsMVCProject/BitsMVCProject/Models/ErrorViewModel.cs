f47bafff7d752cff2de98bc79c77609b40cfaf0a
MyroslavKl <muroslavkl@gmail.com>
2025-09-03 13:09:33 +0300

    created basic mvc project

---
+++ BitsMVCProject/BitsMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BitsMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
